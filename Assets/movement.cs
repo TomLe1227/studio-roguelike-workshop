25572b785eef55e6d03ee1e0ad5717a4b6859592
TomLe1227 <2005thomasle@gmail.com>
2024-10-21 18:24:22 -0700

    commiting

---
+++ Assets/movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Debug = UnityEngine.Debug;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Cat is falling");
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 dir = value.Get<Vector2>();
+        Debug.Log(dir);
+        rb.velocity = dir * 20; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
